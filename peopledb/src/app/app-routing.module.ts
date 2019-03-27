5c1329e9424e2aae707ec1cbffdf2e8fd069d43f
ssebs <ssebs@ssebs-pc.ssebs.com>
2019-03-26 17:50:22 -0700

    styling

--- peopledb/src/app/app-routing.module.ts
+++ peopledb/src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { PersonDetailComponent } from './person-detail/person-detail.component';
+import { PersonSearchComponent } from './person-search/person-search.component';
+import { HomeComponent } from './home/home.component';
+import { PersonCreateComponent } from './person-create/person-create.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'about', component: AboutComponent},
+  { path: 'contact', component: ContactComponent},
+  { path: 'detail/:id', component: PersonDetailComponent},
+  { path: 'create', component: PersonCreateComponent},
+  { path: 'search', component: PersonSearchComponent},
+  { path: 'home', component: HomeComponent},
+  { path: '', component: HomeComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- peopledb/src/app/app.module.ts
+++ peopledb/src/app/app.module.ts
@@ -3,10 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { PersonCreateComponent } from './person-create/person-create.component';
+import { PersonDetailComponent } from './person-detail/person-detail.component';
+import { PersonSearchComponent } from './person-search/person-search.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    PersonCreateComponent,
+    PersonDetailComponent,
+    PersonSearchComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
